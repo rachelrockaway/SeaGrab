d6428117612c119f3fef4be42c8501d390f106d0
Rachel <rachel@rachelrockaway.com>
2015-11-29 01:49:38 -0500

    Update GameViewController code.

--- SeaGrab/GameViewController.swift
+++ SeaGrab/GameViewController.swift
@@ -11,43 +11,33 @@ import SpriteKit
 
 class GameViewController: UIViewController {
     
+        var scene: GameScene!
+    
+    override func prefersStatusBarHidden()-> Bool {
+        return true
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
             // Configure the view.
             let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
+            skView.multipleTouchEnabled = false
+        
+            //Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
             scene.scaleMode = .AspectFill
-            
+        
+            //Present the scene.
             skView.presentScene(scene)
         }
     }
 
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
-}
